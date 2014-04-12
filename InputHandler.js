3ad3b222e66ac8cd4c8b80fc2c4adca356f56b75
unknown <contact@darkhurricanestudio.com>
2014-04-12 17:29:05 +0200

    testt

--- InputHandler.js
+++
@@ -1,9 +0,0 @@
-var keys = new Array();
-
-document.addEventListener('keydown', function(evt) {
-    keys[evt.keyCode] = true;
-});
-
-document.addEventListener('keyup', function(evt) {
-    keys[evt.keyCode] = false;
-});
