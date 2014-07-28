aaa05cd6cf4dbd45d7b9d3c969adfa1375b6d9e5
Avignak <avignak.horain@gmail.com>
2014-07-28 15:55:45 +0200

    beautifier

--- game/dhsLogoTransition.js
+++ game/dhsLogoTransition.js
@@ -9,15 +9,15 @@ DHSLogoTransition.init = function() {
 }
 
 DHSLogoTransition.coreUpdate = function() {
-    var tmp = Main.getStateTimer() / MAIN_STATE_DHS_LOGO_TRANSITION_TIMER ;
-     if (tmp > 1 ) {
+    var tmp = Main.getStateTimer() / MAIN_STATE_DHS_LOGO_TRANSITION_TIMER;
+    if (tmp > 1) {
         tmp = 1;
-     } else {
+    } else {
         if (tmp > 0.5) {
             tmp = 1 - tmp;
-        } 
-     }
-    this.opacity = tmp * 2 * (MAIN_STATE_DHS_LOGO_TRANSITION_TIMER/MAIN_STATE_DHS_LOGO_TRANSITION_STATIC_TIME);
+        }
+    }
+    this.opacity = tmp * 2 * (MAIN_STATE_DHS_LOGO_TRANSITION_TIMER / MAIN_STATE_DHS_LOGO_TRANSITION_STATIC_TIME);
 }
 
 DHSLogoTransition.graphicalUpdate = function() {
