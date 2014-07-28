9c82d0309d1ee68e9f60b6f49b896642bdc5e5f3
unknown <rkebaili@280L050.dudelange.guardian.corp>
2014-07-28 15:50:48 +0200

    Logo DHS done

--- game/dhsLogoTransition.js
+++ game/dhsLogoTransition.js
@@ -9,7 +9,15 @@ DHSLogoTransition.init = function() {
 }
 
 DHSLogoTransition.coreUpdate = function() {
-    this.opacity = (Main.getStateTimer() / MAIN_STATE_DHS_LOGO_TRANSITION_TIMER);
+    var tmp = Main.getStateTimer() / MAIN_STATE_DHS_LOGO_TRANSITION_TIMER ;
+     if (tmp > 1 ) {
+        tmp = 1;
+     } else {
+        if (tmp > 0.5) {
+            tmp = 1 - tmp;
+        } 
+     }
+    this.opacity = tmp * 2 * (MAIN_STATE_DHS_LOGO_TRANSITION_TIMER/MAIN_STATE_DHS_LOGO_TRANSITION_STATIC_TIME);
 }
 
 DHSLogoTransition.graphicalUpdate = function() {
